ae435a74bc528df80306ffadd5ce6b01723b9d32
FrancescoCG <frank-97@hotmail.gr>
2016-11-19 07:22:39 +0200

    Add crazyactive governor (all credits to CrazyGamerGR)

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -497,6 +497,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKACTIVE)
 extern struct cpufreq_governor cpufreq_gov_yankactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CRAZYACTIVE)
+extern struct cpufreq_governor cpufreq_gov_crazyactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_crazyactive)
 #endif
 
 
