d453aee9afa8bb8ea5055449919502f661da5df6
FrancescoCG <frank-97@hotmail.gr>
2016-12-05 03:16:34 +0200

    change cpu overclock/underclock limit

--- include/linux/cpufreq_limit.h
+++ include/linux/cpufreq_limit.h
@@ -14,12 +14,8 @@ struct cpufreq_limit_handle;
 
 #ifdef CONFIG_CPU_FREQ_LIMIT
 
-#if defined(CONFIG_ARCH_MSM8974PRO)
-#define MAX_FREQ_LIMIT	2457600
-#else
-#define MAX_FREQ_LIMIT	2265600
-#endif
-#define MIN_FREQ_LIMIT	300000
+#define MAX_FREQ_LIMIT	2841600
+#define MIN_FREQ_LIMIT	268800
 
 struct cpufreq_limit_handle *cpufreq_limit_get(unsigned long min_freq,
 		unsigned long max_freq, char *label);
